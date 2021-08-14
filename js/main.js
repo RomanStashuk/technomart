a9d1c54855889925d890e95cfafdcf3202a22ece
RomanStashuk <romanstashuk1@gmail.com>
2021-08-14 22:36:07 +0300

    add global variables

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let writeUsBtn = document.querySelector('.contacts__btn');
+let mapBtn = document.querySelector('.contacts__link');
+let addToBookmarkBtn = document.querySelectorAll('.products__btn_to-bookmark');
+
+
