53f518004bc8856d9ce80fcc13061ed7b3051864
RomanStashuk <romanstashuk1@gmail.com>
2022-07-06 16:01:40 +0300

    add script for open modal-write-us & modal-map

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-let writeUsBtn = document.querySelector('.contacts__btn');
-let mapBtn = document.querySelector('.contacts__link');
-let addToBookmarkBtn = document.querySelectorAll('.products__btn_to-bookmark');
+const modalOverlay = document.querySelector('.overlay');
+
+const writeUsLink = document.querySelector('.contacts__btn');
+const writeUsModal = document.querySelector('.write-us');
+const closeWriteUs = writeUsModal.querySelector('.modal__close');
+const writeUsInputName = writeUsModal.querySelector('[name=write-us--name]')
+
+const mapLink = document.querySelector('.contacts__link');
+const mapModal = document.querySelector('.map');
+const closeMap = mapModal.querySelector('.modal__close');
+
+const addToBookmarkBtn = document.querySelectorAll('.products__btn_to-bookmark');
+const addToCartkBtn = document.querySelectorAll('.products__btn_buy');
 
 
+writeUsLink.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    writeUsModal.classList.add('modal--opened');
+    writeUsInputName.focus();
+});
+
+closeWriteUs.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    writeUsModal.classList.remove('modal--opened');
+});
+
+
+mapLink.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    mapModal.classList.add('modal--opened');
+});
+
+closeMap.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    mapModal.classList.remove('modal--opened');
+});
